9dc789d8a1cd78cc97680656db485253cef4b53f
Peter A. Akinlolu <peterakinlolu1@gmail.com>
2022-08-09 11:48:25 +0100

    conditiioned loginSuccess

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import {
 
 const router = Router();
 router.get('/user/verified/:secretToken', verifyUser);
-router.get('/', loginSuccess);
-router.get('/fail-login', loginFail);
+router.get('/ext/auth/login', loginSuccess);
+router.get('/ext/auth/fail-login', loginFail);
 
 export default router
